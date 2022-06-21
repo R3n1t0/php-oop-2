84293c6de54a16aa0dd1d7024331a942e439fdc6
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-06-21 22:23:59 +0200

    classe user

--- class/User.php
+++ class/User.php
@@ -7,13 +7,45 @@
     private $adress;
     private $card_expiration;
 
-    public function __construct($_name, $_surname, $_adress, ){
+    public function __construct($_name, $_surname, $_adress, $_card_expiration){
 
-    
+      $this->name = $_name;
+      $this->surname = $_surname;
+      $this->adress = $_adress;
+      $this->card_expiration = $_card_expiration;
       
     }
+
+    #Setter
+    public function setName($_name){
+      $this->name = $_name;
+    }
+    public function setSurname($_surname){
+      $this->surname = $_surname;
+    }
+    public function setAdress($_adress){
+      $this->adress = $_adress;
+    }
+    public function setCardExpiration($_card_expiration){
+      $this->card_expiration = $_card_expiration;
+    }
+    #
+
+    #Getter
+    public function getName(){
+      return $this->name;
+    }
+    public function getSurname(){
+      return $this->surname;
+    }
+    public function getAdress(){
+      return $this->adress;
+    }
+    public function getCardExpiration(){
+      return $this->_card_expiration;
+    }
+    #
+
   }
-
-
 
 ?>
